33692c5460f8de83287dfdd91818afa9c6cd00af
Tasnim Nahar <tln2007@nyu.edu>
2021-03-31 17:46:51 -0400

    Final update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let affirmations = ["hi", "bye", "cool", "baby", "love", "world"];
+let affirmations = ["thank you for existing", "you are part of a light so bright",
+"live today for you", "take things one step at a time", "you are doing great", "i am proud of you",
+"be kinder than necessary", "deep breaths!", "you are enough", "it's just a bad day, not a bad life",
+"what's meant to be will always find its way", "you are worthy of love", "your value is not based on your productivity", "stay close to people who feel like sunshine",
+"self love isn't selfish", "better days are coming", "the best is yet to come", "it's okay to not be okay", "hang in there", "go at your own pace"];
 
 var quoteElement;
 var quoteButtonElement;
